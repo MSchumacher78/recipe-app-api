77d9635a444d9a26636fdf903a5dc9a67c72b7b5
Mschumacher78 <mschuma78@gmail.com>
2024-06-18 22:58:14 +0200

    Add Waiting for DB to CI/CD

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,6 +2,7 @@
 Sample Tests
 """
 
+
 from django.test import SimpleTestCase
 
 from app import calc
@@ -13,11 +14,10 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
         """Test subtracting numbers """
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
